70e90b80bf06e495b0370db1f1c127e8ae833461
JgdGhub <JgdGhub@octatec.plus.com>
2016-06-08 16:55:07 +0100

    added licence

--- scdds/src/uk/co/octatec/scdds/cache/CacheListener.java
+++ scdds/src/uk/co/octatec/scdds/cache/CacheListener.java
@@ -1,5 +1,18 @@
 package uk.co.octatec.scdds.cache;
+/*
+  SC/DDS - simple cached data distribution service
 
+  Copyright 2016 by Jeromy Drake
+
+  This program is free software; you may redistribute and/or modify it under
+  the terms of the GNU General Public License Version 2 as published by the
+  Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  for complete details.
+*/
 import uk.co.octatec.scdds.cache.subscribe.ImmutableCache;
 
 import java.util.Map;
--- scdds/test/uk/co/octatec/scdds/net/DefaultSerializerTest.java
+++ scdds/test/uk/co/octatec/scdds/net/DefaultSerializerTest.java
@@ -1,5 +1,18 @@
 package uk.co.octatec.scdds.net;
+/*
+  SC/DDS - simple cached data distribution service
 
+  Copyright 2016 by Jeromy Drake
+
+  This program is free software; you may redistribute and/or modify it under
+  the terms of the GNU General Public License Version 2 as published by the
+  Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  for complete details.
+*/
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
