882d1033be4f15d30c06ccfe975ab6d96b9bcffa
JgdGhub <JgdGhub@octatec.plus.com>
2016-06-09 07:49:19 +0100

    added comments

--- scdds/src/uk/co/octatec/scdds/GlobalDefaults.java
+++ scdds/src/uk/co/octatec/scdds/GlobalDefaults.java
@@ -31,9 +31,14 @@ public class GlobalDefaults {
 
     // these are just defaults, a threader factory of your own choosing can be  passed in to the publishing cache builder
 
-    public static int numberOfNetworkSendThreads  = 0;
+    public static int numberOfNetworkSendThreads  = 6;
                                         // 0 == single-threaded (without a queue) which could be better in some situations
-                                        // The threader queues up the network writes to be processed by a thread pool
+                                        // The threader queues up the network writes to be processed by a thread pool.
+                                        // If this is set to 1, a single thread is used to send messageas to the network, but
+                                        // the thread services a queue, if it is set to 0, there is no queue and  the
+                                        // sending takes place on the cache's event-notification thread,
+                                        // a value of zero might be useful for high-volume publishers with only a few
+                                        // subscribers
 
 
     public static final  ThreaderFactory threaderFactory = new ThreaderFactoryImpl(numberOfNetworkSendThreads);
